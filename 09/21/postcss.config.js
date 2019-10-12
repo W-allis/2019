7e7c12e07a6d34fb76dbc9eb76888fdc3f76901d
wallis_lin <17317504517@163.com>
2019-10-12 17:45:09 +0800

    修改 autoprefixer css 兼容性插件

--- 09/21/postcss.config.js
+++ 09/21/postcss.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     // parser: 'sugarss',
-    plugins: {
-      'postcss-import': {},
-      'autoprefixer': {}
-    }
+    plugins: [
+      require('postcss-import')({
+      }),
+      require('autoprefixer')({
+        overrideBrowserslist: ['last 2 versions']      //必须设置支持的浏览器才会自动添加添加浏览器兼容
+      })
+    ]
   }
